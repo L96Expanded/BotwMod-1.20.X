67016f104960a9e2dcbf8affa5b551840e587b45
Shadowjumper3000 <96634974+Shadowjumper3000@users.noreply.github.com>
2023-12-05 15:47:34 +0100

    Update ModItems.java

--- src/main/java/net/Barope/botwmod/Item/ModItems.java
+++ src/main/java/net/Barope/botwmod/Item/ModItems.java
@@ -1,5 +1,7 @@
 package net.Barope.botwmod.Item;
 
+import org.apache.http.config.Registry;
+
 import net.Barope.botwmod.BotwMod;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -11,10 +13,12 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, BotwMod.MOD_ID);
 
-    public static final RegistryObject<Item> KOROKSEED = ITEMS.register("korokseed",
-            () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> SPIRITORB= ITEMS.register("spiritorb",
-            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> KOROKSEED = ITEMS.register("korokseed", 
+        () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> SPIRITORB= ITEMS.register("spiritorb", 
+        () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> SPIRITBOW= ITEMS.register("spiritbow",
+        () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
